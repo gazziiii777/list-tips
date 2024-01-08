8c1b5dd8262b3983e1cf67507138ef858fb85f70
michelangeloiiii777 <michelangeloiiii777@gmail.com>
2024-01-08 20:25:01 +0300

    Добавил файекции, которые присылаеют сообщения админу, когда бот запускается/останавливается

--- main.py
+++ main.py
@@ -4,18 +4,29 @@ from aiogram import Bot, Dispatcher, types
 
 BOT_TOKEN = ''
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
+
 dp = Dispatcher()
 
 
-@dp.message()
-async def echo(message: types.Message):
-    await message.answer(text=message.text)
+async def start_bot():
+    await bot.send_message(chat_id='', text='Бот запущен')
+
+
+async def stop_bot():
+    await bot.send_message(chat_id='', text='Бот остановлен')
 
 
 async def main():
-    logging.basicConfig(level=logging.INFO)
-    await dp.start_polling(bot)
+    logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w",
+                        format="%(asctime)s %(levelname)s %(message)s")
+
+    dp.startup.register(start_bot)  # Отправляет сообщение админу когда бот запускается
+    dp.shutdown.register(start_bot)  # Отправляет сообщение админу когда бот останавлявается
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
 
 
 if __name__ == '__main__':
