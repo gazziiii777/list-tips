2ad66ca7d4e8bac877326a83688308ee913023a1
michelangeloiiii777 <michelangeloiiii777@gmail.com>
2024-01-10 14:00:24 +0300

    добавил админку

---
+++ core/admin_panel/admin_commands.py
@@ -0,0 +1,14 @@
+from core.dispatcher import dp
+from aiogram.filters import Command
+from aiogram.types import Message
+from core.settings import settings
+from core.admin_panel.admin_keyboard import admin_keyboard  # Клавиатура для администратора
+
+
+@dp.message(Command('admin'))
+async def cmd_admin(message: Message):
+    if message.from_user.id == settings.bots.admin_id:
+        await message.answer(
+            "Админ панель",
+            reply_markup=admin_keyboard,
+        )
---
+++ core/admin_panel/admin_keyboard.py
@@ -0,0 +1,14 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+# Создаем объекты инлайн-кнопок
+buttons = [
+    [
+        InlineKeyboardButton(text="Рассылка всем пользователям бота", callback_data="distribution_bot")
+    ],
+    [
+        InlineKeyboardButton(text='Документация Telegram Bot API', url='https://core.telegram.org/bots/api')
+    ],
+]
+
+# Создаем объект инлайн-клавиатуры
+admin_keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
